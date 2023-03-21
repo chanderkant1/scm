b33400078b7c2d9a32130e30cae72160ddedd1ac
Chirag Singla <122335953+Chirag7979@users.noreply.github.com>
2023-03-21 15:05:48 +0530

    done sum and sub

--- git-project.c
+++ git-project.c
@@ -1,9 +1,22 @@
 #include<stdio.h>
+void sum (int a,int b);
+void sub (int a,int b);
 int main(){
     int a,b;
     mul(a,b);
     div(a,b);
     sum(a,b);
     sub(a,b);
+   
+    
     return 0;
 }
+void sum (int a,int b){
+    int sum;
+    sum=a+b;
+    printf("%d"sum);}
+void sub (int a,int b){
+    int sub;
+    sub=a-b;
+    printf("%d"sub);}
+    
